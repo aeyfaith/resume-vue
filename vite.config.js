cf5a60e376fa70c0a2b39c616c5fedb1151368eb
aeyfaith <144115359+aeyfaith@users.noreply.github.com>
2023-10-16 01:03:21 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/resume-vue/',
+  base: '/vue-resume-template',
   plugins: [vue()],
 })
